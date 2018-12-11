8a20778b93fdf0814d1982f11f2e5e2ab636d728
ReBelllls <45486603+ReBelllls@users.noreply.github.com>
2018-12-12 00:20:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,4 +45,35 @@ client.on("message", message => {
 };     
 });
 
+
+client.on('message', message => {
+var prefix = "$"
+    if(message.content.startsWith (prefix  + 'members')) {
+        if(!message.channel.guild) return;
+      let embed = new Discord.RichEmbed()
+      .setColor('RANDOM')
+            .setThumbnail(message.author.avatarURL)
+        .setFooter(message.author.username, message.author.avatarURL) 
+  
+      .setDescription(`**:battery: حالة اعضاء السيرفر**
+  **:green_heart: Online**  **[ ${message.guild.members.filter(m=>m.presence.status == 'online').size} ]**
+  **:yellow_heart: Idle**       **[ ${message.guild.members.filter(m=>m.presence.status == 'idle').size} ]**  
+  **:heart: DND**     **[ ${message.guild.members.filter(m=>m.presence.status == 'dnd').size} ]**
+  **:black_heart: Offline** **[ ${message.guild.members.filter(m=>m.presence.status == 'offline').size} ]** `)
+  
+          message.channel.send()
+  
+  message.channel.sendEmbed(embed)
+  }
+  });
+
+
+
+	
+
+	
+	
+	
+
+
 client.login(process.env.BOT_TOKEN);
